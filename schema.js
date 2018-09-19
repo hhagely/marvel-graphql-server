5658aa9c40515b9d8d985e84606c68f682fa3265
Herb Hagely <herb.hagely@gmail.com>
2018-09-19 12:09:32 -0500

    More Api calls
    Added methods for singular character, comic, and event api calls.

--- schema.js
+++ schema.js
@@ -5,9 +5,12 @@ import { AuthenticationError } from 'apollo-server';
 
 export const RootQuery = `
 	type Query {
-		getCharacters(nameStartsWith: String, limit: Int, offset: Int): CharacterDataWrapper
+		getCharacters(nameStartsWith: String, limit: Int, offset: Int): CharacterDataWrapper,
+		getCharacter(characterId: Int): CharacterDataWrapper,
 		getComics(titleStartsWith: String, limit: Int, offset: Int): ComicDataWrapper,
+		getComic(comicId: Int): ComicDataWrapper,
 		getEvents(nameStartsWith: String, limit: Int, offset: Int): EventDataWrapper,
+		getEvent(eventId: Int): EventDataWrapper,
 		authenticationError: String
 	}
 `;
@@ -45,6 +48,23 @@ export const resolvers = {
 					console.log(err);
 				});
 		},
+		getCharacter: (parent, { characterId }, context, info) => {
+			return axios
+				.get(`http://gateway.marvel.com/v1/public/characters/${characterId}`, {
+					params: {
+						ts,
+						apikey,
+						hash
+					}
+				})
+				.then((response) => {
+					console.log(response.data);
+					return response.data;
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
 		getComics: (parent, { titleStartsWith, limit, offset }, context, info) => {
 			return axios
 				.get('http://gateway.marvel.com/v1/public/comics', {
@@ -65,6 +85,17 @@ export const resolvers = {
 					console.log(err);
 				});
 		},
+		getComic: (parent, { comicId }, context, info) => {
+			return axios
+				.get(`http://gateway.marvel.com/v1/public/comics/${comicId}`)
+				.then((response) => {
+					console.log(response.data);
+					return response.data;
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
 		getEvents: (parent, { nameStartsWith, limit, offset }, context, info) => {
 			return axios
 				.get('http://gateway.marvel.com/v1/public/events', {
@@ -77,6 +108,17 @@ export const resolvers = {
 						offset
 					}
 				})
+				.then((response) => {
+					console.log(response.data);
+					return response.data;
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		},
+		getEvent: (parent, { eventId }, context, info) => {
+			return axios
+				.get(`http://gateway.marvel.com/v1/public/events/${eventId}`)
 				.then((response) => {
 					console.log(response.data);
 					return response.data;
